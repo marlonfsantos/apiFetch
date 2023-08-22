2a7b57011cfbcf90f436096e60fa248de80785c3
Marlon Ferreira dos Santos <marlonfsantos@outlook.com.br>
2023-08-22 14:30:45 -0300

    Update viacep.js

--- viacep.js
+++ viacep.js
@@ -1,15 +1,14 @@
-<script>
-    // Adiciona um ouvinte de evento ao botão
+// Adiciona um ouvinte de evento ao botão
 document.getElementById("fetchButton").addEventListener("click", fazerRequisicao);
-    // Função para fazer a requisição usando a API Fetch
+// Função para fazer a requisição usando a API Fetch
 function fazerRequisicao() {
     // Faz a requisição GET para a URL especificada
-    fetch('https://viacep.com.br/ws/63033-320/json/')
-    .then(response => response.json())
-    .then(data => {
-        const resultadoDiv = document.getElementById("resultado");
-        // Exibe os dados do JSON na div de resultado
-        resultadoDiv.innerHTML = `
+    fetch('https://viacep.com.br/ws/95701-208/json/')
+        .then(response => response.json())
+        .then(data => {
+            const resultadoDiv = document.getElementById("resultado");
+            // Exibe os dados do JSON na div de resultado
+            resultadoDiv.innerHTML = `
             <p>CEP: ${data.cep}</p>
             <p>Logradouro: ${data.logradouro}</p>
             <p>Complemento: ${data.complemento}</p>
@@ -20,11 +19,11 @@ function fazerRequisicao() {
             <p>GIA: ${data.gia}</p>
             <p>DDD: ${data.ddd}</p>
             <p>SIAFI: ${data.siafi}</p>
+            
         `;
-    })
-    // Exibe mensagem para console de erro
-    .catch(error => {
-        console.error('Ocorreu um erro:', error);
-    });
+        })
+        // Exibe mensagem para console de erro
+        .catch(error => {
+            console.error('Ocorreu um erro:', error);
+        });
 }
-</script>
